daf2e18be9e99718680a6c924fc5797e8013c717
Simone Ferronato <simoneferronato0221@gmail.com>
2023-09-18 13:27:42 +0100

    crie a conta bancaria

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: conhecer e importar a classe Scanner
+            Scanner sc = new Scanner(System.in);
+		    int numero;
+		    String agencia;
+		    String nomeCliente;
+		    double saldo;
+
+        // Exibir as mensagens para o nosso usuário
+            System.out.println("---------------CONTA BANCÁRIA---------------");
+		    System.out.println();
+		    
+
+        // Obter pela scanner os valores digitados no teminal
+            System.out.println("Por favor, digite o número da Conta: (1021)");
+		    numero = sc.nextInt();
+
+            System.out.println("Agora digite o número da Agência: (067-8)");
+		    agencia = sc.next();
+
+		    System.out.println("Me informe seu nome: ");
+		    nomeCliente = sc.next();
+		    sc.nextLine();
+
+		    System.out.println("Agora me informa seu saldo atual: ");
+		    saldo = sc.nextDouble();
+
+        // Exbir a mensagem da conta criada
+            System.out.println();
+		    System.out.printf("Olá %s, obrigado por criar uma conta em "
+				+ "nosso banco, sua agência é %s, "
+				+ "conta %d e seu saldo %.2f já está disponível"
+				+ " para saque.", nomeCliente, agencia, numero, saldo);
+
+		    sc.close();
     }
 }
